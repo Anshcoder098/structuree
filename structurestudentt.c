dd94a71b21876970badd8ca467fd0c791ef527b4
Anshcoder098 <ansh88179@gmail.com>
2024-12-06 12:25:42 +0530

    Add files via upload

---
+++ structurestudentt.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+// Define a structure for student details
+struct Student {
+    char name[50];
+    int rollNumber;
+    float marks;
+};
+
+int main() {
+
+    struct Student student;
+
+
+    printf("Enter student name: ");
+    fgets(student.name, sizeof(student.name), stdin);
+
+    printf("Enter roll number: ");
+    scanf("%d", &student.rollNumber);
+
+    printf("Enter marks: ");
+    scanf("%f", &student.marks);
+
+
+    printf("\nStudent Details:\n");
+    printf("Name: %s", student.name);
+    printf("Roll Number: %d\n", student.rollNumber);
+    printf("Marks: %.2f\n", student.marks);
+
+    return 0;
+}
